341efc90f6ccbebc1193c4870a3f77588942f4b5
Shisir <Sisirkhanal20@gmail.com>
2022-02-11 17:04:58 +0545

    logic

--- lib/utils/todoList_widget.dart
+++ lib/utils/todoList_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:todo/provider/todo_provider.dart';
 import 'package:todo/utils/todo_widget.dart';
@@ -10,29 +11,36 @@ class TodoListWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final provider = Provider.of<TodoProvider>(context);
     final todos = provider.todos;
-    return Container(
-      decoration: BoxDecoration(
-          color: Colors.grey[200],
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.grey[300]!,
-                offset: const Offset(4, 5),
-                blurRadius: 15,
-                spreadRadius: 1)
-          ]),
-      child: ListView.separated(
-        separatorBuilder: (context, index) => const SizedBox(height: 10),
-        physics: const BouncingScrollPhysics(),
-        padding: const EdgeInsets.all(10),
-        itemCount: todos.length,
-        itemBuilder: (context, index) {
-          final todo = todos[index];
-          return TodoWidget(
-            todo: todo,
+    return todos.isEmpty
+        ? Center(
+            child: Text(
+              'Sorry! No todos',
+              style: GoogleFonts.rubik(fontSize: 20),
+            ),
+          )
+        : Container(
+            decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey[300]!,
+                      offset: const Offset(4, 5),
+                      blurRadius: 15,
+                      spreadRadius: 1)
+                ]),
+            child: ListView.separated(
+              separatorBuilder: (context, index) => const SizedBox(height: 10),
+              physics: const BouncingScrollPhysics(),
+              padding: const EdgeInsets.all(10),
+              itemCount: todos.length,
+              itemBuilder: (context, index) {
+                final todo = todos[index];
+                return TodoWidget(
+                  todo: todo,
+                );
+              },
+            ),
           );
-        },
-      ),
-    );
   }
 }
